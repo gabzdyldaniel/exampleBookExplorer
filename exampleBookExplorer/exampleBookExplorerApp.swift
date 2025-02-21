2d188cca5a896f6bf8d492d31a1dfc654929fb40
Daniel Gabzdyl <daniel.gabzdyl@eman.cz>
2025-02-21 14:27:43 +0100

    chore: Initialize new SwiftUI project
    - Created a new SwiftUI project with default settings.
    
    This commit represents the starting point for development.

---
+++ exampleBookExplorer/exampleBookExplorerApp.swift
@@ -0,0 +1,14 @@
+//
+// Created with ❤️ by Daniel Gabzdyl.
+
+import SwiftUI
+
+
+@main
+struct exampleBookExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ exampleBookExplorerTests/exampleBookExplorerTests.swift
@@ -0,0 +1,14 @@
+//
+// Created with ❤️ by Daniel Gabzdyl.
+
+import Testing
+@testable import exampleBookExplorer
+
+
+struct exampleBookExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
